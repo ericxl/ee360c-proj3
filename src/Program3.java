c6bf8d9f1d697f30de221fb5f6165efc081becbc
Eric Liang <jklelxy@gmail.com>
2016-12-01 02:04:10 -0600

    initial commit

---
+++ src/Program3.java
@@ -0,0 +1,32 @@
+/**
+ * Name: Xiaoyong Liang
+ * EID: XL5432
+ */
+public class Program3 implements IProgram3{
+
+    private int numClasses;
+    private int maxGrade;
+    private GradeFunction gf;
+
+    public Program3() {
+        this.numClasses = 0;
+        this.maxGrade = 0;
+        this.gf = null;
+    }
+
+    public void initialize(int numClasses, int maxGrade, GradeFunction gf){
+        return;
+        /* Your Code Here! */
+    }
+
+    public int[] computeHours(int totalHours){
+        return new int[0];
+        /* Your Code Here! */
+    }
+
+    public int[] computeGrades(int totalHours){
+        return new int[0];
+        /* Your Code Here! */
+    }
+	
+}
